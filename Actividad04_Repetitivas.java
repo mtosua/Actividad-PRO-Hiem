4d742e0fdb7995c92b73bf65391f58277d30fed5
Hiem Nain <hiemmariangel.nain.munoz@zabalburu.org>
2025-10-17 13:54:23 +0200

    Añadido Actividad04_Repetitivas

---
+++ Actividad04_Repetitivas.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.zabalburu.actividad04_repetitivas;
+
+/**
+ *
+ * @author DAW1
+ */
+public class Actividad04_Repetitivas {
+
+    public static void main(String[] args) {
+        System.out.println("=== Tabla del 5 for ===");
+        for (int c = 1; c <=10 ; c++) {
+        System.out.println("5 x 1 = " + c *(5 * 1) );
+        
+         }
+          System.out.println("=== Tabla del 5 while ===");
+        int c = 1;
+        while (c < 11) {
+            System.out.println("5 x 1 = " + c * (5 * 1) );
+            c++;
+        
+             }
+            
+            System.out.println("=== Table del 5 do while ===");
+        int y = 1;
+        do {
+            System.out.println("5 x 1 = " + y * (5 * 1));
+            y++;
+        } while (y < 11);
+        
+    }
+        
+        
+
+         /* System.out.println("=== Tabla del 6 while ===");
+        int i = 1;
+        while (i < 11) {
+            System.out.println("6 x 1 = " + i * (6 * 1) );
+            i++;
+            
+        }
+            
+            System.out.println("=== Table del 7 do while ===");
+        int j = 1;
+        do {
+            System.out.println("7 x 1 = " + j * (7 * 1));
+            j++;
+        } while (j < 11); */
+
+        }       
+                
+        
+    
+
+
+
+
+
+
